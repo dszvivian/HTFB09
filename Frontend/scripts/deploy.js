ac83bd9ee1264e68b008a048495298ad6c1d024f
dszvivian <dszvivian@gmail.com>
2024-02-20 12:00:47 +0530

    Hardhat initialized

---
+++ Frontend/scripts/deploy.js
@@ -0,0 +1,30 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+async function main() {
+
+  const [inv1,std2] = await ethers.getSigners();
+
+  // Deploy Student Loan
+  const investor = await ethers.getContractFactory('InvestorToken')
+  const investorToken = await investor.deploy(inv1)
+  await investorToken.deployed()
+
+  console.log(`Deployed Real Estate Contract at: ${investorToken.address}`)
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
