d219c9d6b35a326d76d983a9318c3d94aae1fe26
dszvivian <dszvivian@gmail.com>
2024-02-21 07:52:13 +0530

    Final code

--- StuDetails.sol
+++ StuDetails.sol
@@ -3,7 +3,7 @@
 pragma solidity >= 0.8.0 <0.9.0;
 
 //This is only use in Rinkeby testnet
-import "./priceConvertRinkeby.sol"; 
+import "./priceConvert.sol"; 
 
 // Build the Contract for student side 
 contract StuDetails
